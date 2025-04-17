12c138670c7277f6fa16cf0ef388042e7143863b
Anuj Kumar <anujk182002@gmail.com>
2025-04-17 17:13:50 +0530

    final submition

--- Gold_Price_Prediction_WebApp.py
+++ Gold_Price_Prediction_WebApp.py
@@ -11,7 +11,7 @@ import streamlit as st
 #loading. the saved model
 loaded_model = pickle.load(open('gold_price_prediction_model.sav','rb'))
 
-#creating a function for prediction
+
 
 def gold_price_prediction(input_data):
 
